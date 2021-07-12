75c9a9237f321f0947a6a3bfa8044572aa109162
diego alvarez <diego.stonerough@gmail.com>
2021-07-12 13:38:39 -0500

    se removio codigo sin usar

--- src/ResidentContainer.js
+++ src/ResidentContainer.js
@@ -7,7 +7,6 @@ const ResidentContainer = ({residents}) => {
     return (
         <div className="resident-info">
             <h5 className="font-style">RESIDENTS</h5>
-            {/* <p className="font-style">Limited to 10</p> */}
             <div className="main-container">
             {list.slice(0,10)}           
             </div>
